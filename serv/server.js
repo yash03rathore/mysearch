301dec9fbc23a6fe12b828be975b0bc2b4f825d0
yash03rathore <rathoreyash2003@gmail.com>
2024-01-25 19:05:24 -0800

    final updation

--- serv/server.js
+++ serv/server.js
@@ -6,6 +6,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+
 const OPENAI_AI_KEY = process.env.OPENAI_AI_KEY
 
 app.post('/completions', async(req, res) => {
