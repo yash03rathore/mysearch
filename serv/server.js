c0f29fe8d8fad2cdd2a33faf0aec873b80f52343
yash03rathore <rathoreyash2003@gmail.com>
2024-01-25 19:16:55 -0800

    final updation

--- serv/server.js
+++ serv/server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mysearch-3sei.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
