8fa8867b37837d23e1fa918c9fb127e95fea9bc1
yash03rathore <rathoreyash2003@gmail.com>
2024-01-25 19:25:58 -0800

    final updation

--- serv/server.js
+++ serv/server.js
@@ -6,12 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mysearch-3sei.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+
   
 
 const OPENAI_AI_KEY = process.env.OPENAI_AI_KEY
